7399c9c28207e8433602f6c946245aacccf32f9e
xingfengwxx <1099420259@qq.com>
2019-10-08 15:31:25 +0800

    添加新的数据写入

--- write_sqlite.py
+++ write_sqlite.py
@@ -10,6 +10,7 @@
 import json
 import os
 import sqlite3
+import time
 
 
 def write_sqlite():
@@ -24,30 +25,46 @@ def write_sqlite():
         PREVIEW         TEXT   NOT NULL,
         URLS            TEXT   NOT NULL);
         ''')
-        print('Table created successfully')
+        print('PIC_INFO Table created successfully')
+
+        c.execute('''CREATE TABLE IMG_INFO
+        (ID INT PRIMARY KEY    NOT NULL,
+        TITLE           TEXT   NOT NULL,
+        TIMES           TEXT   NOT NULL,
+        IMG_URL         TEXT   NOT NULL,
+        PREVIEW         TEXT   NOT NULL,
+        URLS            TEXT   NOT NULL);
+        ''')
+        print('IMG_INFO Table created successfully')
         conn.commit()
         conn.close()
+    # 已失效
     # 读取json数据
-    with open('back.json', 'r', encoding='utf-8') as f:
-        # 数据有格式化
-        dict_back = json.load(f)
-    with open('test.json', 'r', encoding='utf-8') as f:
-        # 图片更全，使用test文件即可(17年-18年)
-        dict_test = json.load(f)
-    with open('data.json', 'r', encoding='utf-8') as f:
-        # 新的图片地址（18年-19年）
+    # with open('back.json', 'r', encoding='utf-8') as f:
+    #     # 数据有格式化
+    #     dict_back = json.load(f)
+    # with open('test.json', 'r', encoding='utf-8') as f:
+    #     # 图片更全，使用test文件即可(17年-18年)
+    #     dict_test = json.load(f)
+    # with open('data.json', 'r', encoding='utf-8') as f:
+    #     # 新的图片地址（18年-19年）
+    #     dict_data = json.load(f)
+
+    with open('new.json', 'r', encoding='utf-8') as f:
         dict_data = json.load(f)
 
     conn = sqlite3.connect('Girl.db')
     c = conn.cursor()
     # 首先删除表中所有数据
     c.execute('delete from PIC_INFO;')
-    print('len_test=%d, len_data=%d, len_total=%d' %
-          (len(dict_test), len(dict_data), len(dict_test) + len(dict_data)))
+    # print('len_test=%d, len_data=%d, len_total=%d' %
+    #       (len(dict_test), len(dict_data), len(dict_test) + len(dict_data)))
+
     # 自增ID,ID起始数等于前面字典元素个数之和
     num = 0
-    write_file(conn, c, dict_test, num)
-    write_file(conn, c, dict_data, len(dict_test))
+    # write_file(conn, c, dict_test, num)
+    # write_file(conn, c, dict_data, num)
+    write_file_new(conn, c, dict_data)
 
     conn.close()
     print('写入数据库完成')
@@ -66,6 +83,32 @@ def write_file(conn, cursor, dict_data, num):
               (num, key, value['times'], value['img_url'], value['preview'], str_urls)
         cursor.execute(sql)
         conn.commit()
+
+
+def write_file_new(conn, cursor, dict_data):
+    urls = dict_data['图片地址']
+    print('urls length=%d' % len(urls))
+    print('写入数据中...')
+    # 每50张一组
+    table_num = 0
+    num = 0
+    str_urls = ''
+    str_img_url = ''
+    key = ''
+    for str in urls:
+        table_num += 1
+        num += 1
+        str_urls += str
+        str_urls += ';'
+        if num == 50:
+            str_preview = str
+            num = 0
+            str_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+            sql = "INSERT INTO IMG_INFO VALUES(%d, '%s', '%s', '%s', '%s', '%s')" % \
+                  (table_num, key, str_time, str_img_url, str_preview, str_urls)
+            cursor.execute(sql)
+            conn.commit()
+            str_urls = ''
 
 
 if __name__ == '__main__':
